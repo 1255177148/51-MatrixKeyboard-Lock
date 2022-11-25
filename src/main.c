d6e7fb82b60ce32e54bf3f335e2454178c7a62ba
elvis <1255177148@qq.com>
2022-11-25 13:21:48 +0800

    使用字符串接受密码

--- src/main.c
+++ src/main.c
@@ -5,43 +5,41 @@
 
 void main()
 {
-    unsigned int password;
-    // char password[7];
-    // char initPassword[7] = "000000";
-    unsigned int realPassword = 4567;
+    char password[7]     = "000000";
+    char initPassword[7] = "000000";
+    char realPassword[]  = "456789";
     unsigned char martrixKeyNumber;
-    char count = 0;
+    char count = 6;
     LCD_Init();
     LCD_ShowString(1, 1, "PassWord:");
     while (1) {
         martrixKeyNumber = martrixKey();
         if (martrixKeyNumber) {
             if (martrixKeyNumber <= 10) {
-                if (count < 4) {
-                    password *= 10;                    // 密码向左移动一位
-                    password += martrixKeyNumber % 10; // 获取一位密码，这里余10，是因为S10按键要算作0
-                    count++;
+                if (count > 0) {
+                    password[count] = martrixKeyNumber % 10; // 密码向左移动一位，这里余10，是因为S10按键要算作0
+                    count--;
                 }
                 LCD_ShowNum(2, 1, password, 4);
             }
             if (martrixKeyNumber == 11) {
-                if (password == realPassword) {
+                if (strcmp(password, realPassword) == 0) {
                     LCD_ShowString(1, 14, "OK ");
-                    password = 0;
-                    count    = 0;
-                    LCD_ShowNum(2, 1, password, 4);
+                    count = 6;
+                    strcpy(password, initPassword);
+                    LCD_ShowString(2, 1, initPassword);
                 } else {
                     LCD_ShowString(1, 14, "ERR");
-                    password = 0;
-                    count    = 0;
-                    LCD_ShowNum(2, 1, password, 4);
+                    count = 6;
+                    strcpy(password, initPassword);
+                    LCD_ShowString(2, 1, initPassword);
                 }
             }
             if (martrixKeyNumber == 12) {
                 LCD_ShowString(1, 14, "   ");
-                password = 0;
-                count    = 0;
-                LCD_ShowNum(2, 1, password, 4);
+                count = 6;
+                strcpy(password, initPassword);
+                LCD_ShowString(2, 1, initPassword);
             }
         }
     }
